899d9242bdde3c63cda16e37de9e954c6052de9e
Chris <52377842+ChrisSulz@users.noreply.github.com>
2023-09-26 13:10:14 +0200

    adjusted strapi configs for rate-limits

--- public/my-project/config/server.js
+++ public/my-project/config/server.js
@@ -1,10 +1,16 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+  },
+  settings: {
+    rateLimit: {
+      max: 1000000, // Maximale Anzahl der Anfragen
+      timeWindow: 60000, // Zeitfenster in Millisekunden (hier 1 Minute)
+    },
   },
 });
