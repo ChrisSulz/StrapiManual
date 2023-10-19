a7cd84115f2e2369ad3506f5620cead283f75ddc
Chris <52377842+ChrisSulz@users.noreply.github.com>
2023-10-19 23:29:43 +0200

    rotated scenarios "publish / unpublish exhibitor"

--- cypress/e2e/01 Content Manager.cy.js
+++ cypress/e2e/01 Content Manager.cy.js
@@ -3,37 +3,7 @@ describe("01 Content Manager", () => {
     cy.initialise("editor");
   });
 
-  it("A Publishing an exhibitor", () => {
-    cy.visit("https://test.skb-virtuell.de:8080/admin/");
-    cy.get('li:contains("Content Manager")').click();
-    cy.get("li span").contains("Aussteller", { matchCase: false }).click();
-
-    cy.wait(1000);
-    cy.screenshot("contentmanager-aussteller-01", {
-      capture: "viewport",
-      overwrite: true,
-    });
-
-    cy.get("h1").contains("Aussteller");
-    cy.get("main table tbody tr span").contains("Draft").first().click();
-
-    cy.wait(1000);
-    cy.screenshot("contentmanager-aussteller-entry-draft", {
-      capture: "viewport",
-      overwrite: true,
-    });
-
-    // (!) force: true ist notwendig, da das <span>-Element vom <button>-Element überlappt wird
-    cy.get("button span").contains("Publish").click({ force: true });
-
-    cy.wait(1000);
-    cy.screenshot("contentmanager-aussteller-entry-published-01", {
-      capture: "viewport",
-      overwrite: true,
-    });
-  });
-
-  it("B Unpublishing an exhibitor", () => {
+  it("A Unpublishing an exhibitor", () => {
     cy.visit("https://test.skb-virtuell.de:8080/admin/");
     cy.get('li:contains("Content Manager")').click();
     cy.get("li span").contains("Aussteller", { matchCase: false }).click();
@@ -67,6 +37,36 @@ describe("01 Content Manager", () => {
 
     cy.wait(1000);
     cy.screenshot("contentmanager-aussteller-entry-unpublished", {
+      capture: "viewport",
+      overwrite: true,
+    });
+  });
+
+  it("B Publishing an exhibitor", () => {
+    cy.visit("https://test.skb-virtuell.de:8080/admin/");
+    cy.get('li:contains("Content Manager")').click();
+    cy.get("li span").contains("Aussteller", { matchCase: false }).click();
+
+    cy.wait(1000);
+    cy.screenshot("contentmanager-aussteller-01", {
+      capture: "viewport",
+      overwrite: true,
+    });
+
+    cy.get("h1").contains("Aussteller");
+    cy.get("main table tbody tr span").contains("Draft").first().click();
+
+    cy.wait(1000);
+    cy.screenshot("contentmanager-aussteller-entry-draft", {
+      capture: "viewport",
+      overwrite: true,
+    });
+
+    // (!) force: true ist notwendig, da das <span>-Element vom <button>-Element überlappt wird
+    cy.get("button span").contains("Publish").click({ force: true });
+
+    cy.wait(1000);
+    cy.screenshot("contentmanager-aussteller-entry-published-01", {
       capture: "viewport",
       overwrite: true,
     });
